1ea0b74c0c9d1e504e528a459be6cda62edf13f0
ArasxDD <arasxdd@gmail.com>
2020-12-12 14:51:56 +0100

    Dodano projekty mobilne

---
+++ CzadRuletMobile/CzadRuletMobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CzadRuletMobile.ViewModels;
+using CzadRuletMobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CzadRuletMobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ CzadRuletMobile/CzadRuletMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CzadRuletMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CzadRuletMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
